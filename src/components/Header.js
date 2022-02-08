27ea78b2d8561bc98617e01fce8ed1405a9cba00
Isabela Correia <isabela.correiasilva00@gmail.com>
2022-02-07 22:27:29 -0300

    Req 3 - parcial

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,17 +1,44 @@
 import React, { Component } from 'react';
+import Loading from '../pages/Loading';
+import { getUser } from '../services/userAPI';
 
 class Header extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      userName: 'Convidado',
+      loading: false,
+    };
+    this.callsUser = this.callsUser.bind(this);
+  }
+
+  componentDidMount() {
+    const { userName } = this.state;
+    this.callsUser();
+    console.log('cdm:', userName);
+  }
+
+  callsUser = async () => {
+    this.setState({ loading: true });
+    const newUser = await getUser().name;
+    console.log('callsuser:', newUser);
+
+    this.setState(() => ({ userName: newUser, loading: false }));
+  };
+
   render() {
+    const { loading, userName } = this.state;
+
     return (
       <div data-testid="header-component">
-        <p>Cabeçalho</p>
+        {loading && <Loading />}
+        <p data-testid="header-user-name">{userName}</p>
       </div>
     );
   }
 }
 
-Header.propTypes = {
-
-};
+Header.propTypes = {};
 
 export default Header;
