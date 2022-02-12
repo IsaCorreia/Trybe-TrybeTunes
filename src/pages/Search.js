f58e06a8b7c01396033bd4e4d0ca515defb0409f
Isabela Correia <isabela.correiasilva00@gmail.com>
2022-02-12 14:32:21 -0300

    Requisito 5

--- src/pages/Search.js
+++ src/pages/Search.js
@@ -2,11 +2,38 @@ import React, { Component } from 'react';
 import Header from '../components/Header';
 
 class Search extends Component {
+  state = {
+    isButtonDisabled: true,
+  };
+
+  onNameInputChange = (event) => {
+    const { value } = event.target;
+    const MIN_STRING_LENGTH = 2;
+    const buttonState = !(value.length >= MIN_STRING_LENGTH || false);
+    this.setState({
+      isButtonDisabled: buttonState,
+      // userName: value,
+    });
+  };
+
   render() {
+    const { isButtonDisabled } = this.state;
     return (
       <div data-testid="page-search">
         <Header />
-        <h1>Search 🔎</h1>
+        <h1>Pesquisar 🔎</h1>
+        <input
+          data-testid="search-artist-input"
+          type="text"
+          onChange={ this.onNameInputChange }
+        />
+        <button
+          data-testid="search-artist-button"
+          type="button"
+          disabled={ isButtonDisabled }
+        >
+          Pesquisar
+        </button>
       </div>
     );
   }
